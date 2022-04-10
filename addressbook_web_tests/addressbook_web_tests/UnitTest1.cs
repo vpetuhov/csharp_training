793563b58d6ca2b54f4ae01762886a722f7f7a1e
vpetukhov <phoenixxx59@gmail.com>
2022-04-10 15:59:20 +0500

    Проект создан

---
+++ addressbook_web_tests/addressbook_web_tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace addressbook_web_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
