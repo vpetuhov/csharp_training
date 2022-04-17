5e870fa207eecd59366f0cc461a1216dca67f758
vpetukhov <phoenixxx59@gmail.com>
2022-04-18 00:51:24 +0500

    Создание вспомогательных методов и классов

--- addressbook_web_tests/addressbook_web_tests/GroupCreationTests.cs
+++ addressbook_web_tests/addressbook_web_tests/GroupCreationTests.cs
@@ -42,28 +42,67 @@ namespace WebAddressbookTests
         [Test]
         public void GroupCreationTest()
         {
-            driver.Navigate().GoToUrl(baseURL);
-            driver.FindElement(By.Name("user")).Click();
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.Name("new")).Click();
-            driver.FindElement(By.Name("group_name")).Click();
-            driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("q");
-            driver.FindElement(By.Name("group_header")).Click();
-            driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("q");
-            driver.FindElement(By.Name("group_footer")).Click();
-            driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("q");
-            driver.FindElement(By.Name("submit")).Click();
+            OpenHomePage();
+            Login(new AccountData("admin", "secret"));
+            GoToGroupsPage();
+            InitGroupCreation();
+            GroupData group = new GroupData("aaa");
+            group.Header = "ddd";
+            group.Footer = "sss";
+            FillGroupForm(group);
+            SubmitGroupCreation();
+            ReturnToGroupsPage();
+        }
+
+        private void ReturnToGroupsPage()
+        {
             driver.FindElement(By.LinkText("group page")).Click();
             driver.FindElement(By.LinkText("Logout")).Click();
         }
+
+        private void SubmitGroupCreation()
+        {
+            driver.FindElement(By.Name("submit")).Click();
+        }
+
+        private void FillGroupForm(GroupData group)
+        {
+            driver.FindElement(By.Name("group_name")).Click();
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(group.Name);
+            driver.FindElement(By.Name("group_header")).Click();
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(group.Header);
+            driver.FindElement(By.Name("group_footer")).Click();
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(group.Footer);
+        }
+
+        private void InitGroupCreation()
+        {
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void GoToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Click();
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL);
+        }
+
         private bool IsElementPresent(By by)
         {
             try
--- addressbook_web_tests/addressbook_web_tests/UnitTest1.cs
+++ addressbook_web_tests/addressbook_web_tests/UnitTest1.cs
@@ -4,15 +4,41 @@ namespace addressbook_web_tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void TestMethodSquare()
         {
+            Square s1 = new Square(5);
+            Square s2 = new Square(10);
+            Square s3 = s1;
+
+            Assert.AreEqual(s1.Size, 5);
+            Assert.AreEqual(s2.Size, 10);
+            Assert.AreEqual(s3.Size, 5);
+
+            s3.Size = 15;
+
+            Assert.AreEqual(s1.Size, 15);
+
+            s2.Colored = true;
         }
 
         [Test]
-        public void Test1()
+        public void TestMethodCircle()
         {
-            Assert.Pass();
+            Circle s1 = new Circle(5);
+            Circle s2 = new Circle(10);
+            Circle s3 = s1;
+
+            Assert.AreEqual(s1.Radius, 5);
+            Assert.AreEqual(s2.Radius, 10);
+            Assert.AreEqual(s3.Radius, 5);
+
+            s3.Radius = 15;
+
+            Assert.AreEqual(s1.Radius, 15);
+
+            s2.Colored = true;
+
         }
     }
 }
