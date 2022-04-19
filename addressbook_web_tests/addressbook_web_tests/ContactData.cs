34e0395dec14a8f9a0c73437e2c8b0c846f3f72b
vpetukhov <phoenixxx59@gmail.com>
2022-04-19 23:13:01 +0500

    Создание вспомогательных методов и классов для контактов

---
+++ addressbook_web_tests/addressbook_web_tests/ContactData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    internal class ContactData
+    {
+        private string firstName;
+        private string lastName;
+
+        public ContactData(string firstName, string lastname)
+        {
+            this.firstName = firstName;
+            this.lastName = lastname;
+        }
+
+        public string FirstName
+        {
+            get 
+            { 
+                return firstName; 
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+    }
+}
