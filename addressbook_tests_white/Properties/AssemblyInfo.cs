7d8709587ace7524a213709d11f8514f70063150
vpetukhov <phoenixxx59@gmail.com>
2022-06-14 01:34:25 +0500

    Реализованы тесты для добавления и удаления групп при помощи White

---
+++ addressbook_tests_white/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("addressbook_tests_white")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("addressbook_tests_white")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d3a1f576-2fe2-49ab-9580-909ae26931ca")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ addressbook_tests_white/appmanager/HelperBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace addressbook_tests_white
+{
+    public class HelperBase
+    {
+        protected ApplicationManager manager;
+
+        public HelperBase(ApplicationManager manager)
+        {
+            this.manager = manager;
+        }
+    }
+}
